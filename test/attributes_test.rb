da9b2a4b33fbf2310e270bf996616c3bbaf834e6
Kevin Deisz <kevin.deisz@gmail.com>
2017-01-26 11:40:57 -0500

    Handle booleans appropriately

--- test/attributes_test.rb
+++ test/attributes_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class AttributesTest < Minitest::Test
   def test_booleans?
-    skip 'Querying booleans not functioning properly yet'
     assert_equal true, Todo.first.published?
     assert_equal false, Todo.last.published?
   end
