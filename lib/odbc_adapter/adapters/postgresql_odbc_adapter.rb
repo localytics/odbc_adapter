d1d5f8b190d41d5679d7f5e4fb73b7d78b123102
Kevin Deisz <kevin.deisz@gmail.com>
2017-02-10 11:21:26 -0500

    Correct function name for filtering

--- lib/odbc_adapter/adapters/postgresql_odbc_adapter.rb
+++ lib/odbc_adapter/adapters/postgresql_odbc_adapter.rb
@@ -19,7 +19,7 @@ module ODBCAdapter
 
       # Filter for ODBCAdapter#tables
       # Omits table from #tables if table_filter returns true
-      def table_filter(schema_name, table_type)
+      def table_filtered?(schema_name, table_type)
         %w[information_schema pg_catalog].include?(schema_name) || table_type !~ /TABLE/i
       end
 
