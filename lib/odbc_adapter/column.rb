abdfd4593514d395c35b98c01bd5330e008f82d9
Shehbaz <shehbazbashir@gmail.com>
2020-09-29 08:04:33 +0100

    Upgrade to rails 6

--- lib/odbc_adapter/column.rb
+++ lib/odbc_adapter/column.rb
@@ -5,8 +5,8 @@ module ODBCAdapter
     # Add the native_type accessor to allow the native DBMS to report back what
     # it uses to represent the column internally.
     # rubocop:disable Metrics/ParameterLists
-    def initialize(name, default, sql_type_metadata = nil, null = true, table_name = nil, native_type = nil, default_function = nil, collation = nil)
-      super(name, default, sql_type_metadata, null, table_name, default_function, collation)
+    def initialize(name, default, sql_type_metadata = nil, null = true, native_type = nil, default_function = nil)
+      super(name, default, sql_type_metadata, null, default_function)
       @native_type = native_type
     end
   end
--- odbc_adapter.gemspec
+++ odbc_adapter.gemspec
@@ -19,7 +19,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
-  spec.add_dependency 'activerecord', '>= 5.2.1'
+  spec.add_dependency 'activerecord', '>= 6.0.1'
   spec.add_dependency 'ruby-odbc', '~> 0.9'
 
   spec.add_development_dependency 'bundler', '~> 1.14'
